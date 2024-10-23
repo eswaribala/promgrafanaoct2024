9aaa1c979b2ee25d87ce5dbe26dfc343b1a5c55f
eswaribala <Parameswaribala@gmail.com>
2024-10-23 14:37:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ app.post('/webhook', (req, res) => {
     console.log('Received Webhook:', req.body);
     res.status(200).send('Webhook received');
 });
-
+app.get('/', (req, res) => {
+    
+    res.status(200).send('Webhook ready to rock');
+});
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
